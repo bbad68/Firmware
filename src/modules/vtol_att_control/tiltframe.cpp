57d171a39d1e2daa41983081642ec4fe7d9930d9
Jonathan Lee <jon@indrorobotics.com>
2015-10-27 15:11:38 -0700

    Implement TiltFrame State Functions. Update TiltFrame with functionality to update VTOL state machine and system outputs.

--- src/modules/vtol_att_control/tiltframe.cpp
+++ src/modules/vtol_att_control/tiltframe.cpp
@@ -83,7 +83,133 @@ TiltFrame::~TiltFrame()
  */
 void TiltFrame::update_vtol_state()
 {
-	// TODO.
+	parameters_update();
+
+	/**
+	 * The state of the VTOL is determined by the position of a two position
+	 * switch, which sets the desired Flight Mode, and the angle of the motor
+	 * frame relative to the target forward speed (set via either pilot or
+	 * software control).
+	 */
+	if (_attc->is_fixed_wing_requested() == true)
+	{
+		/**
+		 * Switch is currently set to Fixed Wing Mode. Now check current
+		 * state to determine required transition action(s).
+		 */
+		switch (_vtol_schedule.flight_mode)
+		{
+		case MC_MODE:
+			/* Begin transition to FW Mode. */
+	        _vtol_schedule.flight_mode = TRANSITION_TO_FW;
+	        _vtol_schedule.trans_start = hrt_absolute_time();
+	        break;
+	    case FW_MODE:
+	        /* Maintain flight in FW Mode. */
+	        if ((_frame_angle <
+					_params_tiltframe.fw_centre_angle -
+					_params_tiltframe.max_angle_range) ||
+	            (_frame_angle >
+	                _params_tiltframe.fw_centre_angle +
+	                _params_tiltframe.max_angle_range))
+	        {
+	            /**
+	             * Motor frame outside bounds of FW Mode range, revert to
+	             * corresponding Transition mode.
+	             */
+	            _vtol_schedule.flight_mode = TRANSITION_TO_FW;
+	        }
+	        break;
+	    case TRANSITION_TO_FW:
+	        /* Continue the Transition to FW Mode. */
+	        if ((_frame_angle >=
+	                _params_tiltframe.fw_centre_angle) &&
+	        	(_frame_angle <=
+	                _params_tiltframe.fw_centre_angle +
+	                _params_tiltframe.max_angle_range))
+	        {
+	            /* Transition complete, frame within FW Mode range. */
+	            _vtol_schedule.flight_mode = FW_MODE;
+	            _vtol_schedule.trans_end = hrt_absolute_time();
+	        }
+	        break;
+	    case TRANSITION_TO_MC:
+	        /**
+	         * Switch was flipped during Transition to MC. Stop previous
+	         * transition attempt and begin Transition to FW.
+	         */
+	        _vtol_schedule.flight_mode = TRANSITION_TO_FW;
+	        _vtol_schedule.trans_start = hrt_absolute_time();
+	        break;
+	    }
+	}
+	else if (_attc->is_fixed_wing_requested() == false)
+	{
+	    /**
+	     * Switch is currently set to Multi-Copter Mode. Now check current
+	     * state to determine required transition action(s).
+		 */
+        switch (_vtol_schedule.flight_mode)
+        {
+        case MC_MODE:
+            /* Maintain flight in MC Mode. */
+            if ((_frame_angle <
+                    _params_tiltframe.mc_centre_angle -
+                    _params_tiltframe.max_angle_range) ||
+                (_frame_angle >
+                    _params_tiltframe.mc_centre_angle +
+                    _params_tiltframe.max_angle_range))
+            {
+                /**
+                 * Motor frame outside bounds of MC Mode range, revert to
+                 * corresponding Transition mode.
+                 */
+                _vtol_schedule.flight_mode = TRANSITION_TO_MC;
+            }
+            break;
+        case FW_MODE:
+            /* Begin transition to MC Mode. */
+            _vtol_schedule.flight_mode = TRANSITION_TO_MC;
+            _vtol_schedule.trans_start = hrt_absolute_time();
+            break;
+        case TRANSITION_TO_FW:
+            /**
+             * Switch was flipped during Transition to FW. Stop previous
+             * transition attempt and begin Transition to MC.
+             */
+            _vtol_schedule.flight_mode = TRANSITION_TO_MC;
+            _vtol_schedule.trans_start = hrt_absolute_time();
+            break;
+        case TRANSITION_TO_MC:
+            /* Continue the Transition to MC Mode. */
+            if ((_frame_angle >=
+                    _params_tiltframe.mc_centre_angle -
+                    _params_tiltframe.max_angle_range) &&
+                (_frame_angle <=
+                	_params_tiltframe.mc_centre_angle))
+            {
+                /* Transition complete, frame within MC Mode range. */
+                _vtol_schedule.flight_mode = MC_MODE;
+                _vtol_schedule.trans_end = hrt_absolute_time();
+            }
+            break;
+        }
+    }
+
+    /* Map specific control phases to simple control modes. */
+    switch (_vtol_schedule.flight_mode)
+    {
+    case MC_MODE:
+        _vtol_mode = ROTARY_WING;
+        break;
+    case FW_MODE:
+        _vtol_mode = FIXED_WING;
+        break;
+    case TRANSITION_TO_FW:
+    case TRANSITION_TO_MC:
+        _vtol_mode = TRANSITION;
+        break;
+    }
 }
 
 /**
@@ -91,7 +217,25 @@ void TiltFrame::update_vtol_state()
  */
 void TiltFrame::update_mc_state()
 {
-	// TODO.
+	if (_vtol_schedule.flight_mode == MC_MODE)
+	{
+		/**
+		 * Change motor frame angle to counter pitch input from pilot.
+		 * Intended operation is to have fuselage remain level while the
+		 * aircraft moves forward with a tilted frame.
+		 */
+		_angle_diff =
+			(_actuators_mc_in->control[actuator_controls_s::INDEX_PITCH] *
+			_params_tiltframe.max_angle_range) - _frame_angle;
+		_angle_step = math::constrain(
+			_angle_diff,
+			-(_params_tiltframe.max_angle_step),
+			_params_tiltframe.max_angle_step);
+		_frame_angle += _angle_step;
+
+		/* Explicitly set MC Control Weights to One (full MC Control). */
+		set_mc_weights(1.0f);
+	}
 }
 
 /**
@@ -99,7 +243,26 @@ void TiltFrame::update_mc_state()
  */
 void TiltFrame::update_fw_state()
 {
-	// TODO.
+	if (_vtol_schedule.flight_mode == FW_MODE)
+    {
+        /**
+         * Change motor frame angle based on desired forward velocity.
+         * Intended operation is to have frame tilt forward in proportion
+         * to throttle, in order to steadily increase fraction of lift
+         * generated by the wings.
+         */
+        _angle_diff =
+            (_actuators_fw_in->control[actuator_controls_s::INDEX_THROTTLE] *
+            _params_tiltframe.max_angle_range) - _frame_angle;
+        _angle_step = math::constrain(
+            _angle_diff,
+            -(_params_tiltframe.max_angle_step),
+            _params_tiltframe.max_angle_step);
+        _frame_angle += _angle_step;
+
+        /* Explicitly set MC Control Weights to Zero (full FW Control). */
+        set_mc_weights(0.0f);
+    }
 }
 
 /**
@@ -107,7 +270,26 @@ void TiltFrame::update_fw_state()
  */
 void TiltFrame::update_transition_state()
 {
-	// TODO.
+	if (_vtol_mode == TRANSITION)
+    {
+        /* Change motor frame angle incrementally during transition states. */
+        if (_vtol_schedule.flight_mode == TRANSITION_TO_FW)
+        {
+            _angle_diff = _params_tiltframe.fw_centre_angle - _frame_angle;
+        }
+        else if (_vtol_schedule.flight_mode == TRANSITION_TO_MC)
+        {
+            _angle_diff = _params_tiltframe.mc_centre_angle - _frame_angle;
+        }
+        _angle_step = math::constrain(
+            _angle_diff,
+            -(_params_tiltframe.max_angle_step),
+            _params_tiltframe.max_angle_step);
+        _frame_angle += _angle_step;
+
+        /* Update control weighting based on new frame angle. */
+        set_mc_weights(cos(_frame_angle * ANG_TO_RAD));
+    }
 }
 
 /**
